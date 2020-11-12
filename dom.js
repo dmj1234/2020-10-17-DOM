c0ff0eac7b3f6a6c7c709ac240c7924e33b3b112
灰一样的感觉 <63853697+dmj1234@users.noreply.github.com>
2020-11-12 18:27:15 +0800

    Add files via upload

--- dom.js
+++ dom.js
@@ -1,2 +1,22 @@
-const div = dom.creat("<td>hi</td>");
-console.log(div);
+window.dom = {
+    find(selector, scope) {
+        return (scope || document).querySelectorAll(selector);
+    },
+    style(node, name, value) {
+        if (arguments.length === 3) {
+            node.style[name] = value;
+        } else if (arguments.length === 2) {
+            if (typeof name === "string") {
+                return node.style[name];
+            } else if (name instanceof Object) {
+                const object = name;
+                for (let key in object) {
+                    node.style[key] = object[key];
+                }
+            }
+        }
+    },
+    each(nodeList, fn) {
+        for (let i = 0; i < nodeList.length; i++) fn.call(null, nodeList[i]);
+    },
+};
